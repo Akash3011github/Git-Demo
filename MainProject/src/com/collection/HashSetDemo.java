f773cdf6e0f7c127fef088335dafa29a2d6fecfa
Akash3011github <akashdaundkar2001@gmail.com>
2022-11-10 11:44:40 +0530

    Added code for hashSetDemo

---
+++ MainProject/src/com/collection/HashSetDemo.java
@@ -0,0 +1,35 @@
+package com.collection;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetDemo {
+
+	
+	
+	
+	
+    public static void main(String []args)
+    {
+    	
+    	Set<String> set =new HashSet<String>();
+    	
+    	
+    	set.add("Akash");
+    	set.add("Sujal");
+    	set.add("Pravin");
+    	set.add("Vishal");
+    	set.add("Prasad");
+    	
+    	
+    	
+    	for(String str: set) {
+    	
+    	
+    	
+    	System.out.println(str);
+    	
+    	
+    	
+    }
+}}
